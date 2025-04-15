29f41929b97e1c390b802b8fbd53b1f7712a6d33
Exercícios.py2 <joaorafaelsantos17@gmail.com>
2025-04-15 18:45:41 -0300

    Add initial HTML, CSS, and JavaScript files for the website

---
+++ file 1/index.js
@@ -0,0 +1,42 @@
+//console.log("Hello, World!");
+//console.log("This is a test.");
+
+//window.alert("this is an alert!");
+//window.alert("this is another alert!");
+
+// this is a comment /* this is a comment */
+
+//document.getElementById("H1").textContent = "Hello";
+//document.getElementById("P").textContent = "This is a test.";
+
+// variable = A container that stores a value
+//            Behaves as if it were the value it contains
+
+// 1. declaration  lex x;
+// 2. assigment    x = 100;
+
+
+let x = 100; // declaration and assignment
+let y;
+y = 200; // assignment
+let z = x + y; // declaration and assignment
+console.log(`The sum is ${z} and the value of x is ${x} and the value of y is ${y}`);
+console.log(typeof x); // number
+
+
+let FirstName = "João"; // string
+let SecondName = "Rafael"; // string
+let Age = 18; // number
+let isStudent = true; // boolean
+let people = FirstName + " " + SecondName; // string
+
+document.getElementById("p1").textContent =`The name is ${people} and the age is ${Age}`;
+document.getElementById("p2").textContent = `${people} is a student: ${isStudent}`;
+
+
+let online = true; // boolean
+console.log(`João is online: ${online}`);
+let forSale = false; // boolean
+console.log(`This car for sale: ${forSale}`);
+
+
