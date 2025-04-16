a7bffc84053af96f550ac76fc0f080d019c43c8e
Exercícios.py2 <joaorafaelsantos17@gmail.com>
2025-04-16 10:47:04 -0300

    Refactor HTML structure and enhance JavaScript functionality for user input handling

--- file 1/index.js
+++ file 1/index.js
@@ -15,7 +15,7 @@
 // 1. declaration  lex x;
 // 2. assigment    x = 100;
 
-
+/*
 let x = 100; // declaration and assignment
 let y;
 y = 200; // assignment
@@ -38,5 +38,41 @@ let online = true; // boolean
 console.log(`João is online: ${online}`);
 let forSale = false; // boolean
 console.log(`This car for sale: ${forSale}`);
+*/
+
+
+// Arithmetic Operators = ( + - * / ** % )
+// ex. 11 = x + 5;
+
+//students /= 2;
+//students *= 2;
+//students++;  soma 1 a variável students
+//students--;  subtrai 1 a variável students
+
+// Operator precedence
+// 1. Parentheses ()
+// 2. Exponentiation **
+// 3. Multiplication and Division * / %
+// 4. Addition and Subtraction + -
+
+
+// How to accept user input
+// 1. Easy Way = window.prompt("Enter your name: ");
+// 2. Professsional Way = html textbox
+
+let username;
+document.getElementById("submit").onclick = function(){
+    username = document.getElementById("input1").value;
+    document.getElementById("h1").textContent = `Hello ${username}`;
+}
+
+// type conversion = change the datatype of a value to another
+//                   (strings, numbers, booleans)
+
+let age = window.prompt("Qual sua idade?");
+age = Number(age); // converte para number
+age+=1; // =181 porem se usar age++; funciona
+console.log(age); // string
+
 
 
