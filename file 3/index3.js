578614854fd628d4ef9a3e8880755e3155504b2d
Exercícios.py2 <joaorafaelsantos17@gmail.com>
2025-07-30 11:07:18 -0300

    Refactor index3.js to uncomment and organize while loop examples; improve code clarity and structure.

--- file 3/index3.js
+++ file 3/index3.js
@@ -9,4 +9,54 @@
 
 
 
+// While loop
 
+/*
+let username = "";
+
+do{
+    username = window.prompt("Please enter your username:");
+}while( username == "" || username == null)
+console.log(`Welcome, ${username}!`);
+
+
+
+let loggedIn = false;
+let username;
+let password;
+
+do{
+    username = window.prompt("Enter your username:");
+    password = window.prompt("Enter your password:");
+
+    if(username === "JR" && password === "123"){
+        loggedIn = true;
+        console.log('You are logged in!');
+        
+    }
+    else{
+        console.log("Invalid credentials! Please try again.");
+    }
+}while(!loggedIn)
+*/
+
+
+
+// For loop = repeat some code a LIMITED amount of times
+
+/*
+for(let i = 10; i > 0; i-=2){
+    console.log(i);
+}console.log("Happy new year!");
+*/
+
+
+for(let i = 1; i <= 20; i++){
+
+    if(i == 13){
+        continue; // Skip the number 13
+    }
+    else{
+        console.log(i);
+    }
+}
